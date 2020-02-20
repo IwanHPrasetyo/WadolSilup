b53d94a8aefd34a5aae1fc668b34b81fc323cd4c
Iwan H Prasetyo <55027286+IwanHPrasetyo@users.noreply.github.com>
2020-02-20 22:31:48 +0700

    'setup'

---
+++ src/page/startup/startup.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {Container, Header, Content, Text, View} from 'native-base';
+
+export default class startup extends Component {
+  render() {
+    return (
+      <Container>
+        <Header></Header>
+        <Content>
+          <Text>okkkk</Text>
+        </Content>
+      </Container>
+    );
+  }
+}
